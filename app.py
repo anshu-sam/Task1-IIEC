0e5bd2f17e19541ed3e3630c4054f431852531b2
anshu-sam <59832097+anshu-sam@users.noreply.github.com>
2020-08-20 10:18:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import webbrowser
 import pyttsx3 as p
 import os
 p.speak("Hello! How may I help you today? ")
-print(" Enter any of the given options or type in your requirements \n 1.Music \n 2.Coding platform \n 3.Web \n 4.Email \n 5.Game \n 6.Notepad \n 7.Media Player \n 8.Entertainment")
+print(" Enter any of the given options or type in your requirements \n 1.Music \n 2.Coding platform \n 3.Web \n 4.Email \n 5.Game \n 6.Notepad \n 7.Media Player \n 8.Entertainment \n 9.Video call")
 while True:
    print("Type in your choice or your requirements!",end=' ')
    s=input()
@@ -10,7 +11,8 @@ while True:
    elif(("Code" in s) or ("code" in s) or ("coding" in s) or ("coding platform" in s) or s=='2'):
           os.system("CodeBlocks")
    elif(("Web" in s) or ("Browser" in s) or ("web" in s) or ("browser" in s) or  s=='3'):
-          os.system("Chrome google.com")
+          p=input("Enter the website you want to visit")
+          webbrowser.open_new_tab(p) 
    elif(("email" in s) or("mail" in s) or s=='4'):
                 if(("email" in s) or("mail" in s) or s=='4'):
                    d=input("Do you want to compose a mail?Enter yes to proceed else enter anything")
@@ -21,14 +23,15 @@ while True:
    elif(("Game" in s) or ("play" in s) or ("game" in s)  or s=='5'):
           os.system("start Steam:")
    elif(("Notepad" in s) or ("editor" in s) or ("notepad" in s) or s=='6'):
-          filename=input("Enter filename with extension")  
-          os.system("start Notepad filename")
+          os.system("Notepad")
    elif(("media player" in s) or ("media" in s) or ("player" in s) or s=='7'):
           os.system("start vlc")
-   elif(("watch" in s) or ("shows" in s) or ("movie" in s) or s=='8'):
+   elif(("watch" in s) or ("shows" in s) or ("movies" in s) or s=='8'):
           os.system("start MOVIES")
+   elif(("video" in s) or ("conference" in s) or ("connect" in s) or ("calling" in s) or (s=='9')):
+          os.system("start Zoom")
    elif(('exit' in s) or ('leave' in s) or ('quit' in s)):
-          p.speak("Thankyou, see you soon")
+          p.speak('Thankyou see you soon')
           break
    else:
           print("Invalid input")
